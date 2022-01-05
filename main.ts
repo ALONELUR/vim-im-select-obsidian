8de7c704fa761f02bc5ac8861061f3a3868711e3
ALONELUR <yinwenhan1998@gmail.com>
2022-01-05 15:09:24 +0800

    Add compatibility with CodeMirror6

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
 
 interface VimImPluginSettings {
 	defaultIM: string;
@@ -46,14 +46,32 @@ export default class VimImPlugin extends Plugin {
 	private currentInsertIM = '';
 	private isWinPlatform = false;
 
+	private initialized = false;
+	private editorMode: 'cm5' | 'cm6' = null;
+
 	async onload() {
 		await this.loadSettings();
-		this.app.workspace.on('codemirror', (cm: CodeMirror.Editor) => {
-			cm.on('vim-mode-change', (modeObj: any) => {
-				if (modeObj)
-					this.onVimModeChanged(modeObj);
-			});
+
+		// when open a file, to initialize current
+		// editor type CodeMirror5 or CodeMirror6
+		this.app.workspace.on('file-open', async (_file) => {
+			if (!this.initialized)
+				await this.initialize();
+
+			let view = this.getActiveView();
+			if (view) {
+				var editor = this.getCodeMirror(view);
+
+				if (editor) {
+					editor.on('vim-mode-change', (modeObj: any) => {
+						if (modeObj) {
+							this.onVimModeChanged(modeObj);
+						}
+					});
+				}
+			}
 		});
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -66,6 +84,35 @@ export default class VimImPlugin extends Plugin {
 		if (this.isWinPlatform) {
 			console.log("VimIm Use Windows config");
 		}
+	}
+
+	async initialize() {
+		if (this.initialized)
+			return;
+
+		// Determine if we have the legacy Obsidian editor (CM5) or the new one (CM6).
+		// This is only available after Obsidian is fully loaded, so we do it as part of the `file-open` event.
+		if ('editor:toggle-source' in (this.app as any).commands.editorCommands) {
+			this.editorMode = 'cm6';
+			console.log('Vimrc plugin: using CodeMirror 6 mode');
+		} else {
+			this.editorMode = 'cm5';
+			console.log('Vimrc plugin: using CodeMirror 5 mode');
+		}
+
+		this.initialized = true;
+	}
+
+	private getActiveView(): MarkdownView {
+		return this.app.workspace.getActiveViewOfType(MarkdownView);
+	}
+
+	private getCodeMirror(view: MarkdownView): CodeMirror.Editor {
+		// For CM6 this actually returns an instance of the object named CodeMirror from cm_adapter of codemirror_vim
+		if (this.editorMode == 'cm6')
+			return (view as any).sourceMode?.cmEditor?.cm?.cm;
+		else
+			return (view as any).sourceMode?.cmEditor;
 	}
 
 	onVimModeChanged(modeObj: any) {
